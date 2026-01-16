7a0ed26c04b670b7c868a3d7973a05a62276f74f
simodewit <117769294+simodewit@users.noreply.github.com>
2026-01-16 19:19:14 +0100

    completed password change loop
    I forgot to add in the page where you end up after changing your password. I now added it so the full system now works.

--- SimRacingPlatform/Pages/PasswordChangedPage.xaml.cs
+++ SimRacingPlatform/Pages/PasswordChangedPage.xaml.cs
@@ -1,30 +1,75 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using SimRacingPlatform.Windows;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace SimRacingPlatform.Pages;
-
-/// <summary>
-/// An empty page that can be used on its own or navigated to within a Frame.
-/// </summary>
-public sealed partial class PasswordChangedPage : Page
+namespace SimRacingPlatform.Pages
 {
-    public PasswordChangedPage()
+    public sealed partial class PasswordChangedPage : Page, INotifyPropertyChanged
     {
-        InitializeComponent();
+        private DispatcherTimer _timer;
+        private int _secondsRemaining = 3;
+
+        private string _countdownText = "Redirecting in 3 seconds...";
+        public string CountdownText
+        {
+            get => _countdownText;
+            set
+            {
+                if (_countdownText != value)
+                {
+                    _countdownText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PasswordChangedPage()
+        {
+            InitializeComponent();
+            DataContext = this;
+            StartCountdown();
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void StartCountdown()
+        {
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+
+            _timer.Tick += OnTimerTick;
+            _timer.Start();
+        }
+
+        private void OnTimerTick(object sender, object e)
+        {
+            _secondsRemaining--;
+
+            if (_secondsRemaining > 0)
+            {
+                CountdownText = $"Redirecting in {_secondsRemaining} seconds...";
+            }
+            else
+            {
+                _timer.Stop();
+                CountdownText = "Redirecting now...";
+                RedirectUser();
+            }
+        }
+
+        private void RedirectUser()
+        {
+            MainWindow.Instance.NavigateTo(typeof(LoginPage));
+        }
     }
 }
